aff5124d2b6f266b414f0a6aa93a75bc9f588719
JUNNA0203 <119775717+JUNNA0203@users.noreply.github.com>
2022-12-11 10:24:07 +0900

    新規作成
    新規作成

---
+++ app.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const app = express();
+const sqlite3 = require("sqlite3");
+var ABC = "";
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+res.sendFile(__dirname + '/public/calendar.html');
+});
+app.get('/outgolist', (req, res) => {
+    res.sendFile(__dirname + '/public/outgolist.html');
+    });
+const db = new sqlite3.Database("./test.db");
+db.serialize(() => {
+    // db.run("drop table if exists members");
+    db.run("create table if not exists members(id integer primary key,ymd,member,name,yen,kosuu)");
+    db.each("select * from members", (err, row) => {
+        console.log(`${row.name}${row.member} ${row.yen} ${row.kosuu}`);
+    });
+    db.all("select * from members", (err, rows) => {
+        ABC = rows;
+    });
+    db.get("select count(*) from members", (err, count) => {
+        console.log(count);
+    })
+});
+
+app.get('/1234', (req, res) => res.json(ABC));
+
+app.get('/touroku', func1);
+function func1(req, res){
+//console.logはなんの値が入っているのかテスト的に確認したいから書いているので、最終的には消してよい
+console.log(req.query.ymd)
+console.log(req.query.member)
+console.log(req.query.name)
+console.log(req.query.yen)
+console.log(req.query.kosuu)
+//DBで登録
+//サーバー側でクエリストリングを取得する書きかた↓
+var getDate=req.query.ymd;
+var getMember=req.query.member;
+var getGoods=req.query.name;
+var getYen=req.query.yen;
+var getKosuu=req.query.kosuu;
+
+db.serialize(() => {
+    // db.run("drop table if exists members");
+    db.run("create table if not exists members(id integer primary key,ymd,member,name,yen,kosuu)");
+    db.run("insert into members(ymd,member,name,yen,kosuu) values(?,?,?,?,?)", getDate,getMember,getGoods,getYen,getKosuu);
+    db.each("select * from members", (err, row) => {
+        console.log(`${row.ymd} ${row.member} ${row.name} ${row.yen} ${row.kosuu}`);
+    });
+    db.all("select * from members", (err, rows) => {
+        console.log(JSON.stringify(rows));
+        ABC = rows;
+    });
+    db.get("select count(*) from members", (err, count) => {
+        console.log(count["count(*)"]);
+    })
+});
+res.sendFile(__dirname + '/public/outgo.html');
+}
+
+app.get('/outgo', (req, res) => {
+  res.sendFile(__dirname + '/public/outgo.html');
+  });
+app.get('/income', (req, res) => {
+    res.sendFile(__dirname + '/public/income.html');
+    });
+app.get('/homegamen', (req, res) => {
+        res.sendFile(__dirname + '/public/homegamen.html');
+        });
+app.listen(3000, () => {
+console.log("connect");
+});
+app.get('/syokika', func2);
+function func2 (req, res){
+    db.serialize(() => {
+       db.run("drop table if exists members");
+       ABC = "";
+    });
+    
+    res.sendFile(__dirname + '/public/homegamen.html')
+};
---
+++ public/js/outgolist.js
@@ -0,0 +1,43 @@
+var goukei = 0;
+    // APIからJSONを取得する
+   fetch('./1234')
+   .then((response) => response.json())
+   .then((ABC) => {
+       // table要素を生成
+var table = document.createElement('table');
+
+// ヘッダーを作成
+var tr = document.createElement('tr');
+for (key in ABC[0]) {
+      // th要素を生成
+      var th = document.createElement('th');
+      // th要素内にテキストを追加
+      th.textContent = key;
+      // th要素をtr要素の子要素に追加
+      tr.appendChild(th);
+      }
+  // tr要素をtable要素の子要素に追加
+  table.appendChild(tr);
+
+// テーブル本体を作成
+for (var i = 0; i < ABC.length; i++) {
+  // tr要素を生成
+  var tr = document.createElement('tr');
+       goukei+=parseInt(ABC[i].yen);
+  // th・td部分のループ
+  for (key in ABC[0]) {
+        // td要素を生成
+        var td = document.createElement('td');
+        // td要素内にテキストを追加
+        td.textContent = ABC[i][key];
+        // td要素をtr要素の子要素に追加
+        tr.appendChild(td);
+      }
+  // tr要素をtable要素の子要素に追加
+  table.appendChild(tr);
+  }
+// 生成したtable要素を追加する
+document.getElementById('maintable').appendChild(table);
+alert(goukei);
+       }
+   )
