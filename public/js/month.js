4c7c3d3116d55a7d5b6471aefb3da90a964169b8
reo-takahashi0225 <120262009+reo-takahashi0225@users.noreply.github.com>
2022-12-11 11:42:34 +0900

    年月別値取得
    機能追加

--- public/js/month.js
+++ public/js/month.js
@@ -1,4 +1,8 @@
 function watch(){
+
   //次の年月別にリスト表表示処理(クエリパラメータで年月日をセットしてあげる　　?以降)
-  window.location.href='./yearmonthwatch';
+  var year = document.getElementById("year").value; //撮ってきた値
+  var month = document.getElementById("month").value;
+  console.log(year,month);
+  // window.location.href='./yearmonthwatch? year = '+year+'&';
 }
